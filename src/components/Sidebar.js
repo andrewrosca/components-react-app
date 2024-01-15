5b4ae6a3e3aa9c098cc81cee5de5cd17cf0ccd83
Andrew Rosca <andrewrosca7@gmail.com>
2024-01-15 13:17:21 -0500

    Added Tables component and Counter component using Reducers

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,6 +6,8 @@ function Sidebar() {
     { label: "Acorrdion", path: "/accordion" },
     { label: "Buttons", path: "/buttons" },
     { label: "Modal", path: "/modal" },
+    { label: "Table", path: "/table" },
+    { label: "Counter", path: "/counter" },
   ];
   const renderedLinks = links.map((link) => {
     return (
